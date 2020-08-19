d28612ca3e3d90157dcef1bfe4414355038156c5
José L. S. A <alagunasalahaddin@live.com>
2020-08-19 22:38:03 +0300

    Setting up the first App version.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react"
+import PropTypes from "prop-types"
+import { Route, Switch } from "react-router"
+import { BrowserRouter } from "react-router-dom"
+import { Container, Grid, Typography, makeStyles } from "@material-ui/core"
 
-function App() {
+import logo from "./logo.svg"
+import "./App.css"
+import { getSearchedFilms } from "./api/films"
+import Films from "./components/views/films"
+import Movie from "./components/views/film"
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    marginTop: theme.spacing(4),
+    marginBottom: theme.spacing(2),
+  },
+  landing: {
+    width: "auto",
+    margin: theme.spacing(4),
+  },
+}))
+
+const App = () => {
+  const classes = useStyles()
+  const [term, setTerm] = useState("Pokemon")
+  const [page, setPage] = useState(1)
+  const [films, setFilms] = useState([])
+
+  const fetchData = async () => {
+    const result = await getSearchedFilms(term, page)
+    setFilms(result.data.Search)
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" render={() => <Films films={films} />} />
+        <Route
+          exact
+          path="/film/:id"
+          render={(props) => <Movie {...props.match.params} />}
+        />
+      </Switch>
+    </BrowserRouter>
+  )
 }
 
-export default App;
+App.propTypes = {
+  match: PropTypes.object.isRequired,
+}
+
+export default App
