156fdd14a7d3589752fc69d20e0970de66882ff4
José L. S. A <alagunasalahaddin@live.com>
2020-08-20 18:55:08 +0300

    Improving film view styles.

--- src/components/views/film.js
+++ src/components/views/film.js
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from "react"
 import PropTypes from "prop-types"
 import moment from "moment"
+import { useHistory } from "react-router"
 import {
   Container,
-  makeStyles,
   Typography,
   Card,
   CardHeader,
@@ -13,17 +13,11 @@ import {
   IconButton,
   Box,
 } from "@material-ui/core"
-import Rating from "@material-ui/lab/Rating"
 import { ArrowBack } from "@material-ui/icons"
+import { Rating, Skeleton } from "@material-ui/lab"
 import { getFilm } from "src/api/films"
-import { useHistory } from "react-router"
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    marginTop: theme.spacing(4),
-    marginBottom: theme.spacing(2),
-  },
-}))
+import "./film.scss"
 
 const Film = ({
   Title,
@@ -40,54 +34,76 @@ const Film = ({
   Country,
   imdbRating,
 }) => {
-  const classes = useStyles()
   let history = useHistory()
 
   return (
-    <Container component="section" spacing={2} className={classes.root}>
-      <Card>
-        <CardHeader
-          title={
-            <>
-              {Title}
-              <Typography component="span" variant="subtitle1" color="textSecondary">
-                ({Year})
+    <Container
+      component="section"
+      spacing={2}
+      style={{ marginTop: 60, marginBottom: 60 }}
+    >
+      <Card className="film">
+        <div>
+          {Poster ? (
+            <CardMedia component="img" image={Poster} title={"Poster: " + Title} />
+          ) : (
+            <Skeleton variant="rect" width="100%">
+              <div style={{ paddingTop: "57%" }} />
+            </Skeleton>
+          )}
+        </div>
+        <div className="right">
+          <CardHeader
+            title={
+              <>
+                {Title}
+                <Typography
+                  component="span"
+                  variant="subtitle1"
+                  color="textSecondary"
+                >
+                  ({Year})
+                </Typography>
+              </>
+            }
+            subheader={
+              <>
+                {Runtime} | {Genre} |{" "}
+                {moment(Released, "DD MMMM YYYY").format("DD MMMM YYYY")} ({Country})
+                | {Rated} |{" "}
+                <Rating
+                  defaultValue={5.0}
+                  value={Number(imdbRating)}
+                  precision={0.1}
+                  max={10}
+                  readOnly
+                />
+              </>
+            }
+          />
+          <CardContent>
+            <Typography component="p" variant="body1" paragraph>
+              {Plot}
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Box>
+              <Typography component="p" variant="body2">
+                <strong>Director:</strong> {Director}
               </Typography>
-            </>
-          }
-          subheader={
-            <>
-              {Runtime} | {Genre} |{" "}
-              {moment(Released, "DD MMMM YYYY").format("DD MMMM YYYY")} ({Country}) |{" "}
-              {Rated} |{" "}
-              <Rating value={Number(imdbRating)} precision={0.1} max={10} readOnly />
-            </>
-          }
-        />
-        <CardMedia image={Poster} title={"Poster: " + Title} />
-        <CardContent>
-          <Typography component="p" variant="body1" paragraph>
-            {Plot}
-          </Typography>
-        </CardContent>
-        <CardActions>
-          <Box>
-            <Typography component="p" variant="body2">
-              <em>Director:</em> {Director}
-            </Typography>
-            <Typography component="p" variant="body2">
-              <em>Writer:</em> {Writer}
-            </Typography>
-            <Typography component="p" variant="body2">
-              <em>Actor:</em> {Actors}
-            </Typography>
-          </Box>
-          <IconButton onClick={() => history.goBack()} aria-label="show more">
-            <ArrowBack />
-          </IconButton>
-        </CardActions>
+              <Typography component="p" variant="body2">
+                <strong>Writer:</strong> {Writer}
+              </Typography>
+              <Typography component="p" variant="body2">
+                <strong>Actor:</strong> {Actors}
+              </Typography>
+            </Box>
+            <IconButton onClick={() => history.goBack()} aria-label="show more">
+              <ArrowBack />
+            </IconButton>
+          </CardActions>
+        </div>
       </Card>
-      <footer></footer>
     </Container>
   )
 }
