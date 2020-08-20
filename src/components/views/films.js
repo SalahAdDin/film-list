d5374383e86ce21cc357157600db9c69b4f4ce4d
José L. S. A <alagunasalahaddin@live.com>
2020-08-20 18:51:43 +0300

    Improving film view styles.

--- src/components/views/films.js
+++ src/components/views/films.js
@@ -1,8 +1,10 @@
 import React, { useState } from "react"
 import PropTypes from "prop-types"
-import { Container, Grid, Typography, makeStyles } from "@material-ui/core"
+import { Container, Typography, makeStyles } from "@material-ui/core"
 import { Pagination } from "@material-ui/lab"
 import FilmCard from "../elements/filmcard"
+
+import "./films.scss"
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -11,15 +13,12 @@ const useStyles = makeStyles((theme) => ({
   },
 }))
 
-const Films = ({ films, currentPage = 1, pageCount = 0, changePage }) => {
+const Films = ({ films, currentPage = 1, pageCount = 0, changePage, term }) => {
   const classes = useStyles()
   const [half, setHalf] = useState(1)
 
   const handlePaginationChange = (event, value) => {
     const newPage = value % 2 === 0 ? Number(value / 2) : Number((value + 1) / 2)
-    console.log("====================================")
-    console.log(value, currentPage, newPage)
-    console.log("====================================")
 
     if (value % 2 === 0 && newPage === currentPage) setHalf(2)
     else if (value % 2 === 0 && newPage !== currentPage) {
@@ -32,11 +31,18 @@ const Films = ({ films, currentPage = 1, pageCount = 0, changePage }) => {
   }
 
   return (
-    <Container component="section" spacing={2} className={classes.root}>
+    <Container component="section" spacing={2} className={classes.root + " films"}>
+      <Typography component="h6" variant="h6" gutterBottom>
+        Results for {term}
+      </Typography>
       {films[half]?.map((film) => (
         <FilmCard key={"movie_" + film.imdbID} {...film} />
       ))}
-      <Pagination count={pageCount} onChange={handlePaginationChange} />
+      <Pagination
+        count={pageCount}
+        onChange={handlePaginationChange}
+        className="pagination"
+      />
     </Container>
   )
 }
@@ -46,6 +52,7 @@ Films.propTypes = {
   currentPage: PropTypes.number,
   pageCount: PropTypes.number,
   changePage: PropTypes.func,
+  term: PropTypes.string,
 }
 
 export default Films
