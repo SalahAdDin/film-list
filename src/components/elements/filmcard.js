4cdbd0e2e9e72247d06899511e899fa37d9c6eaa
José L. S. A <alagunasalahaddin@live.com>
2020-08-20 18:51:07 +0300

    Improving film card component styles.

--- src/components/elements/filmcard.js
+++ src/components/elements/filmcard.js
@@ -1,50 +1,23 @@
 import React from "react"
 import PropTypes from "prop-types"
-import {
-  Card,
-  makeStyles,
-  CardContent,
-  Typography,
-  CardMedia,
-} from "@material-ui/core"
+import { Card, CardMedia, CardContent, Typography } from "@material-ui/core"
 import { Link } from "react-router-dom"
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    display: "flex",
-  },
-  details: {
-    display: "flex",
-    flexDirection: "column",
-  },
-  content: {
-    flex: "1 0 auto",
-  },
-  cover: {
-    width: 151,
-  },
-}))
+import "./filmcard.scss"
 
 const FilmCard = ({ Title, Year, imdbID, Poster }) => {
-  const classes = useStyles()
   return (
     <Link to={"/film/" + imdbID} style={{ textDecoration: "none" }}>
-      <Card className={classes.root}>
-        <div className={classes.details}>
-          <CardContent>
-            <Typography component="h5" variant="h5">
-              {Title}
-            </Typography>
-            <Typography variant="subtitle1" color="textSecondary">
-              {Year}
-            </Typography>
-          </CardContent>
-        </div>
-        <CardMedia
-          className={classes.cover}
-          image={Poster}
-          title={"Poster" + Title}
-        />
+      <Card className="film-card" elevation={0}>
+        <CardContent>
+          <Typography component="h5" variant="h5">
+            {Title}
+          </Typography>
+          <Typography variant="subtitle1" color="textSecondary">
+            {Year}
+          </Typography>
+        </CardContent>
+        <CardMedia className="poster" image={Poster} title={"Poster" + Title} />
       </Card>
     </Link>
   )
